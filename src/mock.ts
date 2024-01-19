b6ae5210bb020606d336c88a3410fe95a7acf798
Nathan Souvignet <nath.souvignet@gmail.com>
2024-01-19 22:25:45 +0100

    fixed type

--- src/mock.ts
+++ src/mock.ts
@@ -3,16 +3,16 @@ import type { Resource } from "./types";
 export function generateMock(): { resources: Resource[]} {
     const bronze = {
         id: 'b1',
-        nom: 'Bronze',
-        qualite: 'impur',
-        logoChemin: 'https://satisfactory.fandom.com/fr/wiki/Minerai_de_cuivre#/media/Fichier:Copper_Ore.png'
+        name: 'Bronze',
+        quality: 'impur',
+        logoPath: 'https://satisfactory.fandom.com/fr/wiki/Minerai_de_cuivre#/media/Fichier:Copper_Ore.png'
     } as Resource
 
     const fer = {
         id: 'f1',
-        nom: 'Fer',
-        qualite: 'normal',
-        logoChemin: 'https://static.wikia.nocookie.net/satisfactory_gamepedia_en/images/8/87/Iron_Ore.png'
+        name: 'Fer',
+        quality: 'normal',
+        logoPath: 'https://static.wikia.nocookie.net/satisfactory_gamepedia_en/images/8/87/Iron_Ore.png'
     } as Resource
 
     return {
